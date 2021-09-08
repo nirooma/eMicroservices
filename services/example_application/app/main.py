7b22e17a2a00887831c77012b484957d210ce3e8
nirmansour <nirooma@icloud.com>
2021-09-08 21:28:41 +0300

    Add code quality job

--- services/example_application/app/main.py
+++ services/example_application/app/main.py
@@ -1,10 +1,10 @@
-from app.core.config import settings
 import datetime
 import platform
 
-from fastapi import Depends, FastAPI, Request, status, Response
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
+from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
@@ -13,15 +13,13 @@ app = FastAPI(
 
 @app.get("/health-check")
 async def health_check():
-    """ Internal use only - Do not use with a client API """
+    """Internal use only - Do not use with a client API"""
 
     return JSONResponse(
         {
             "status": status.HTTP_200_OK,
             "timestamp": datetime.datetime.now().ctime(),
             "debugMode": settings.DEBUG,
-            "OperatingSystem": platform.uname()
+            "OperatingSystem": platform.uname(),
         }
     )
-
-
--- services/example_application/tests/integration/test_web_app/test_main.py
+++ services/example_application/tests/integration/test_web_app/test_main.py
@@ -17,4 +17,3 @@ def test_health_check(client):
     """
     response = client.get("/health-check")
     assert response.status_code == 200
-
