47ef6f8163bbe40a41d3dc63e3f5a9df8243a2c7
nirmansour <nirooma@icloud.com>
2021-09-11 18:22:01 +0300

    logging configuration added

--- services/example-application-service/app/core/config.py
+++ services/example-application-service/app/core/config.py
@@ -1,38 +1,52 @@
 import os
+import platform
 import secrets
 from functools import lru_cache
-
+import enum
 from pydantic import BaseSettings
+
+
+class AppEnvironments(enum.Enum):
+    PRODUCTION = "production"
+    DEVELOPMENT = "development"
+    TEST = "test"
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    PROJECT_NAME: str = "Some_Project"
+    PROJECT_NAME: str = "Cloud Project"
+    DOCKER_MODE: bool = False if platform.uname().system == "Darwin" else True
 
 
 class DevelopmentConfig(Settings):
+    APP_ENVIRONMENT = AppEnvironments.DEVELOPMENT.value
     DEBUG: bool = True
 
 
 class ProductionConfig(Settings):
+    APP_ENVIRONMENT = AppEnvironments.PRODUCTION.value
     DEBUG: bool = False
 
 
-class TestingConfig(Settings):
-    pass
+class TestConfig(Settings):
+    APP_ENVIRONMENT = AppEnvironments.TEST.value
+    DEBUG: bool = True
 
 
 @lru_cache()
 def get_settings():
-    config_cls_dict = {
-        "development": DevelopmentConfig,
+    """
+    Load config based on environment
+    :return:
+    """
+    config_cls = {
         "production": ProductionConfig,
-        "testing": TestingConfig,
+        "development": DevelopmentConfig,
+        "test": TestConfig
     }
 
-    config_name = os.environ.get("SETTINGS_CONFIGURATION", "development")
-    return config_cls_dict[config_name]()
+    return config_cls.get(os.getenv("APP_ENVIRONMENT"), DevelopmentConfig)()
 
 
 settings = get_settings()
--- services/example-application-service/app/main.py
+++ services/example-application-service/app/main.py
@@ -1,25 +1,42 @@
 import datetime
+import logging
 import platform
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
 from fastapi.responses import JSONResponse
 
 from app.core.config import settings
 
-app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-)
+
+logger = logging.getLogger(__name__)
+
+
+def get_app_configuration():
+    application = FastAPI(
+        title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    )
+
+    from app.core.logging import configure_logging
+    configure_logging()
+
+    return application
+
+
+app = get_app_configuration()
 
 
 @app.get("/health-check")
-async def health_check():
+async def health_check(request: Request) -> JSONResponse:
     """Internal use only - Do not use with a client API"""
-
+    print(settings.APP_ENVIRONMENT)
+    logger.info(f"Health check called from IP #{request.scope.get('server')[0]}")
     return JSONResponse(
         {
-            "status": status.HTTP_200_OK,
-            "timestamp": datetime.datetime.now().ctime(),
-            "debugMode": settings.DEBUG,
+            "Status": status.HTTP_200_OK,
+            "Timestamp": datetime.datetime.now().ctime(),
+            "DebugMode": settings.DEBUG,
             "OperatingSystem": platform.uname(),
+            "DockerMode": settings.DOCKER_MODE,
+            "AppMode": settings.APP_ENVIRONMENT
         }
     )
