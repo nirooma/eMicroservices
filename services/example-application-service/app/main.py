ce76a8232a78d3666a75bc3ba9ae3ab284533aa7
nirmansour <nirooma@icloud.com>
2021-09-13 20:56:22 +0300

    templates added to the project

--- services/example-application-service/app/main.py
+++ services/example-application-service/app/main.py
@@ -1,17 +1,28 @@
 import datetime
+import functools
 import logging
+import os
 import platform
-from sqlalchemy.ext.asyncio import AsyncSession
+from types import FunctionType
+from typing import Optional
+
 from fastapi import FastAPI, status, Request, Depends
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
+from pydantic import BaseModel
+
 from app.core.config import settings
-from app.db.session import get_session
-from app.models.user import User
+from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordBearer
 
 logger = logging.getLogger(__name__)
 
+templates = Jinja2Templates(directory="app/templates")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def get_app_configuration():
+
     application = FastAPI(
         title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
     )
@@ -41,11 +52,6 @@ async def health_check(request: Request) -> JSONResponse:
     )
 
 
-@app.post("/users", response_model=User)
-async def add_user(user: User, session: AsyncSession = Depends(get_session)):
-    # user = User(username=user.username, password=user.password)
-    # session.add(user)
-    # await session.commit()
-    # await session.refresh(user)
-    # return 1
-    return {}
+@app.get("/", response_class=HTMLResponse)
+async def hello_world(request: Request):
+    return templates.TemplateResponse("hello_world.html", context={"request": request})
