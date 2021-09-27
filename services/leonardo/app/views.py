19c5e19570c7c447cde314b9262e0d00f989cc27
Nir Mantzur <nirooma@icloud.com>
2021-09-27 20:10:33 +0300

    github actions

--- services/leonardo/app/views.py
+++ services/leonardo/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import datetime
+from django.conf import settings
+import platform
 
-# Create your views here.
+
+def health_check(request) -> JsonResponse:
+    return JsonResponse({
+        "Status": 200,
+        "Timestamp": datetime.datetime.now().ctime(),
+        "DebugMode": settings.DEBUG,
+        "OperatingSystem": platform.uname(),
+        # "DockerMode": settings.DOCKER_MODE,
+        # "AppMode": settings.APP_ENVIRONMENT,
+    })
