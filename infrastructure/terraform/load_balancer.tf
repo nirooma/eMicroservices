4ff459ee7e04c4cce8a9b41711891bef465e259c
Nir Mantzur <nirooma@icloud.com>
2021-10-28 13:16:49 +0300

    default target_group added

--- infrastructure/terraform/load_balancer.tf
+++ infrastructure/terraform/load_balancer.tf
@@ -7,3 +7,21 @@ resource "aws_lb" "load_balancer" {
   subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
 
+# Target group client
+resource "aws_alb_target_group" "default-target-group" {
+  name     = "production-client-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.eMicroservices-vpc.id
+  depends_on = [aws_lb.load_balancer]
+
+#  health_check {
+#    path                = "/health-check/"
+#    port                = "traffic-port"
+#    healthy_threshold   = 2
+#    unhealthy_threshold = 2
+#    timeout             = 2
+#    interval            = 5
+#    matcher             = "200"
+#  }
+}
