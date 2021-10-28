c18df8ea1993f5f460b64da56ef69e7602a72262
Nir Mantzur <nirooma@icloud.com>
2021-10-29 00:48:54 +0300

    aws_alb_target_group and aws_alb_listener added to load_balancer config

--- infrastructure/terraform/global/load_balancer.tf
+++ infrastructure/terraform/global/load_balancer.tf
@@ -5,3 +5,24 @@ resource "aws_lb" "load_balancer" {
   security_groups    = [aws_security_group.load-balancer.id]
   subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
+
+resource "aws_alb_target_group" "default-target-group" {
+  name     = "${var.environment_name}-nginx-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.eMicroservices-vpc.id
+  depends_on = [aws_lb.load_balancer]
+}
+
+# Listener (redirects traffic from the load balancer to the target group)
+resource "aws_alb_listener" "ecs-alb-http-listener" {
+  load_balancer_arn = aws_lb.load_balancer.id
+  port              = "80"
+  protocol          = "HTTP"
+  depends_on        = [aws_alb_target_group.default-target-group]
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
+}
