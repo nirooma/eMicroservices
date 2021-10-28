56604f2c4d3af36a58835300db903e46389495cc
Nir Mantzur <nirooma@icloud.com>
2021-10-29 01:32:40 +0300

    ecs configuration added

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -8,4 +8,7 @@ module "eMicroservices" {
   source = "./modules/eMicroservices"
 
   environment_name = var.environment_name
+  ecs_security_group_id = module.global.ecs_security_group_id
+  private_subnet_1_id = module.global.private_subnet_1_id
+  private_subnet_2_id = module.global.private_subnet_2_id
 }
--- infrastructure/terraform/modules/eMicroservices/variables.tf
+++ infrastructure/terraform/modules/eMicroservices/variables.tf
@@ -2,3 +2,25 @@ variable "environment_name" {
   description = "Name of app environment. Must be unique."
   type        = string
 }
+variable "ecs_security_group_id" {
+  type        = string
+}
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+variable "autoscale_min" {
+  default = 1
+}
+variable "autoscale_max" {
+  default = 3
+}
+variable "autoscale_desired" {
+  default = 1
+}
+variable "private_subnet_1_id" {
+  type = string
+}
+variable "private_subnet_2_id" {
+  type = string
+}
