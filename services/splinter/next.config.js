8720e06eeedd28c1ef522938ad405c85dceb8967
Tomer Levi <18260079+levitomer@users.noreply.github.com>
2021-10-28 19:46:04 +0300

    front end template

--- services/splinter/next.config.js
+++ services/splinter/next.config.js
@@ -1,38 +1,7 @@
-/** @type {import('next').NextConfig} */
+const path = require('path')
+
 module.exports = {
-  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(le|c)ss$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'less-loader',
-          options: {
-            sourceMap: true,
-            lessOptions: {
-              javascriptEnabled: true
-            }
-          }
-        }
-      ]
-    });
-
-    config.plugins.push(
-      new MiniCssExtractPlugin({
-        filename: 'static/css/[name].css',
-        chunkFilename: 'static/css/[contenthash].css'
-      })
-    );
-
-    return config;
-  }
-};
