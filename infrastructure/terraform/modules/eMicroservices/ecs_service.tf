fac61eb0f28d435aec4163ee014e79a969bf307c
Nir Mantzur <nirooma@icloud.com>
2021-10-29 16:15:55 +0300

    aws_ecs_task_definition added

---
+++ infrastructure/terraform/modules/eMicroservices/ecs_service.tf
@@ -0,0 +1,42 @@
+resource "aws_ecs_task_definition" "tommy" {
+  family                = "${var.environment_name}-nginx"
+  container_definitions = <<EOF
+[
+    {
+      "name": "nginx",
+      "image": "nirooma/nginx:latest",
+      "cpu": 1000,
+      "memory": 950,
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80
+        }
+      ],
+    }
+]
+EOF
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+#resource "aws_ecs_service" "talk-booking-service" {
+#  name            = var.environment_name
+#  cluster         = aws_ecs_cluster.eMicroservices-cluster.name
+#  task_definition = aws_ecs_task_definition.tommy.family
+#  iam_role        = aws_iam_role.ecs-service-role.arn
+#  desired_count   = 1
+#  deployment_minimum_healthy_percent = 50
+#
+#  load_balancer {
+#    target_group_arn = aws_alb_target_group.default-target-group.arn
+#    container_name   = "talk-booking-app"
+#    container_port   = var.container_port
+#  }
+#  depends_on = [aws_alb_listener.ecs-alb-http-listener]
+#
+#  lifecycle {
+#    ignore_changes = [task_definition]
+#  }
+#}
