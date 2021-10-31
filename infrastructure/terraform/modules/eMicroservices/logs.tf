290bdb6a712664321136018f74ede4081f994fe6
Nir Mantzur <nirooma@icloud.com>
2021-10-31 22:18:12 +0200

    statiffiles

--- infrastructure/terraform/modules/eMicroservices/logs.tf
+++ infrastructure/terraform/modules/eMicroservices/logs.tf
@@ -25,5 +25,4 @@ resource "aws_cloudwatch_log_stream" "leonardo-db-log-stream" {
 resource "aws_cloudwatch_log_stream" "rabbitmq-log-stream" {
   name           = "${var.environment_name}-rabbitmq"
   log_group_name = aws_cloudwatch_log_group.gen-log-group.name
-
-  
+}
