5e4394a84561e6fbb52a7da47ed48d3f633e259d
Nir Mantzur <nirooma@icloud.com>
2021-11-07 19:57:06 +0200

    change the hosted zone id for the domain

--- infrastructure/terraform/global/domain.tf
+++ infrastructure/terraform/global/domain.tf
@@ -1,15 +1,15 @@
 #resource "aws_route53_zone" "primary" {
 #  name = "yallastam.com"
 #}
-#
-#resource "aws_route53_record" "app" {
-#  zone_id = aws_route53_zone.primary.zone_id
-#  name    = var.domain
-#  type    = "A"
-#
-#  alias {
-#    name                   = aws_lb.load_balancer.dns_name
-#    zone_id                = aws_lb.load_balancer.zone_id
-#    evaluate_target_health = true
-#  }
-#}
+
+resource "aws_route53_record" "app" {
+  zone_id = Z05551602NDS256A9726H
+  name    = var.domain
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.load_balancer.dns_name
+    zone_id                = aws_lb.load_balancer.zone_id
+    evaluate_target_health = true
+  }
+}
